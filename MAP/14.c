a70f4306868dab7ee8f0898cd10d57f3af4f9b0a
Moata Alexandru <alexandru.moata@student.upt.ro>
2023-01-12 12:58:51 +0200

    Read me si adaugare in Git a Dockerfile

---
+++ MAP/14.c
@@ -0,0 +1,65 @@
+﻿/*Se dă o matrice de dimensiunea M x N, generată aleatoriu. Se cere să se calculeze:a) Suma
+elementelor aflate pe diagonala principală, respectiv pe diagonala secundară b) Minimul și
+maximul elementelor aflate sub diagonala principală, respectiv sub diagonala secundară*/
+#include<stdio.h>
+int main()
+{
+	int a[20][20], i, j, m, n;
+	int Suma_principala = 0, Suma_secundara = 0;
+	int Min_princiapala = 32000, Max_principala = -32000;
+	int Min_secundara = 32000, Max_secundara = -32000;
+
+	printf("Valoarea lui m si n este:");
+	scanf("%d", &m);
+	n = m;
+	printf("Matricea este:");
+
+	for (i = 0; i < m; i++)
+		for (j = 0; j < n; j++)
+			scanf("%d", &a[i][j]);
+
+	printf("Matricea:\n");
+	for (i = 0; i < m; i++)
+	    {for (j = 0; j < n; j++)
+			printf("%d ", a[i][j]);
+		printf("\n");
+		}
+
+	for (i = 0; i < m; i++)
+		for (j = 0; j < n; j++)
+		{
+			if (i == j)
+				Suma_principala = Suma_principala + a[i][j];
+			if (i + j == m - 1)
+				Suma_secundara = Suma_secundara + a[i][j];
+		}
+
+	printf("Suma de pe diagonala principala este: %d", Suma_principala);
+	printf("\nSuma de pe diagonala secundara este: %d", Suma_secundara);
+
+	for (i = 0; i < m; i++)
+		for (j = 0; j < n; j++)
+			if (i > j)
+			{
+				if (a[i][j] < Min_princiapala)
+					Min_princiapala = a[i][j];
+				if (a[i][j] > Max_principala)
+					Max_principala = a[i][j];
+			}
+
+	printf("\nMinimul de sub diagonala princiapala este: %d", Min_princiapala);
+	printf("\nMaximul de sub diagonala princapala este: %d", Max_principala);
+
+	for (i = 0; i < m; i++)
+		for (j = 0; j < n; j++)
+			if (i + j > n - 1)
+			{
+				if (a[i][j] < Min_secundara)
+					Min_secundara = a[i][j];
+				if (a[i][j] > Max_secundara)
+					Max_secundara = a[i][j];
+			}
+
+	printf("\nMinimul de sub diagonala secundara este: %d", Min_secundara);
+	printf("\nMaximul de sub diagonala secundara este: %d", Max_secundara);
+}
--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# MAP
+Prin intermediul acestui proiect putem sa calculam suma elementelor aflate pe diagonala principala si pe diagonala secundara,
+dar si minimul si maximul elementelor aflate sub cele doua diagonale.
+Pentru elementele de pe diagonala principala m-am folosit de faptul ca indicele "i" este egala cu indicele "j" si
+pentru cele de pe diagonala secundara de faptul ca "i"+"j" = n-1, unde n este nr de linii si de coloane.
+Elementele aflate sub diagonala principala au indicele "i" mai mic decat indicele "j", iar cele aflate sub
+diagonala secundara au suma indicilor mai mare decat n-1.
+
+Pentru a rula proiectul folositi urmatoarele comenzi:
+docker pull moataav/proiectmap
+docker run -it moataav/proiectmap
